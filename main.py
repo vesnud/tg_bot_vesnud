730c4d29858340ca13855c967eb1cfafc17e2a08
Vesnud <Vesnud@yandex.ru>
2023-04-30 15:41:26 +0300

    демо, начало

---
+++ main.py
@@ -0,0 +1,185 @@
+# Импортируем необходимые классы.
+import logging
+# Добавим необходимый объект из модуля telegram.ext
+from telegram.ext import CommandHandler
+from telegram.ext import Application, MessageHandler, filters
+# from config import TG_BOT_TOKEN
+import datetime as dt
+from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardRemove
+
+TIMER = 5
+TG_BOT_TOKEN = ''
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Определяем функцию-обработчик сообщений.
+# У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
+async def echo(update, context):
+    # У объекта класса Updater есть поле message,
+    # являющееся объектом сообщения.
+    # У message есть поле text, содержащее текст полученного сообщения,
+    # а также метод reply_text(str),
+    # отсылающий ответ пользователю, от которого получено сообщение.
+    await update.message.reply_text(f'Я получил сообщение {update.message.text}')
+
+
+# Напишем соответствующие функции.
+# Их сигнатура и поведение аналогичны обработчикам текстовых сообщений.
+# async def start(update, context):
+#     """Отправляет сообщение когда получена команда /start"""
+#     user = update.effective_user
+#     await update.message.reply_html(
+#         rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",
+#     )
+
+
+async def help_command(update, context):
+    """Отправляет сообщение когда получена команда /help"""
+    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+
+
+async def get_time(update, context):
+    """Отправляет сообщение когда получена команда /help"""
+    await update.message.reply_text(str(dt.datetime.now().time()))
+
+
+async def date_command(update, context):
+    """Отправляет сообщение когда получена команда /help"""
+    await update.message.reply_text(str(dt.datetime.now().date()))
+
+
+def remove_job_if_exists(name, context):
+    """Удаляем задачу по имени.
+    Возвращаем True если задача была успешно удалена."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+# Обычный обработчик, как и те, которыми мы пользовались раньше.
+async def set_timer(update, context):
+    """Добавляем задачу в очередь"""
+    # TIMER = 5
+    global TIMER
+
+    chat_id = update.effective_message.chat_id
+    # Добавляем задачу в очередь
+    # и останавливаем предыдущую (если она была)
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    print(TIMER)
+    if update.message.text.split()[-1].isdigit():
+        TIMER = int(update.message.text.split()[-1])
+    else:
+        TIMER = 5
+    context.job_queue.run_once(task, TIMER, chat_id=chat_id, name=str(chat_id), data=TIMER)
+    print(update.message.text.split()[-1])
+    text = f'Вернусь через {TIMER} с.!'
+    if job_removed:
+        text += ' Старая задача удалена.'
+    await update.effective_message.reply_text(text)
+
+
+async def task(context):
+    """Выводит сообщение"""
+    await context.bot.send_message(context.job.chat_id, text=f'КУКУ! {TIMER}c. прошли!')
+
+
+async def unset(update, context):
+    """Удаляет задачу, если пользователь передумал"""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
+    await update.message.reply_text(text)
+
+
+# Напишем соответствующие функции.
+async def help(update, context):
+    await update.message.reply_text(
+        "Я бот справочник.")
+
+
+async def address(update, context):
+    await update.message.reply_text(
+        "Адрес: г. Москва, ул. Льва Толстого, 16")
+
+
+async def phone(update, context):
+    await update.message.reply_text("Телефон: +7(495)776-3030")
+
+
+async def site(update, context):
+    await update.message.reply_text(
+        "Сайт: http://www.yandex.ru/company")
+
+
+async def work_time(update, context):
+    await update.message.reply_text(
+        "Время работы: круглосуточно.")
+
+
+reply_keyboard = [['/address', '/phone'],
+                  ['/site', '/work_time']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
+
+async def start(update, context):
+    await update.message.reply_text(
+        "Я бот-справочник. Какая информация вам нужна?",
+        reply_markup=markup
+    )
+
+
+async def close_keyboard(update, context):
+    await update.message.reply_text(
+        "Ok",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+
+def main():
+    # Создаём объект Application.
+    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
+    application = Application.builder().token(TG_BOT_TOKEN).build()
+
+    # Создаём обработчик сообщений типа filters.TEXT
+    # из описанной выше асинхронной функции echo()
+    # После регистрации обработчика в приложении
+    # эта асинхронная функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообщений.
+    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
+
+    # Регистрируем обработчик в приложении.
+    application.add_handler(text_handler)
+    # Зарегистрируем их в приложении перед
+    # регистрацией обработчика текстовых сообщений.
+    # Первым параметром конструктора CommandHandler я
+    # вляется название команды.
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("time", get_time))
+    application.add_handler(CommandHandler("date", date_command))
+    application.add_handler(CommandHandler("set_timer", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
+    application.add_handler(CommandHandler("address", address))
+    application.add_handler(CommandHandler("phone", phone))
+    application.add_handler(CommandHandler("site", site))
+    application.add_handler(CommandHandler("work_time", work_time))
+    application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("close", close_keyboard))
+
+    # Запускаем приложение.
+    application.run_polling()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
+if __name__ == '__main__':
+    main()
---
+++ vk_gr.py
@@ -0,0 +1,4 @@
+import vk_api
+
+def set_post(key, id_group, text):
+    pass
